c9bb82731dc9c44cffc2507a6144ae4598df9f5f
Ioannis Karasavvaidis <ioannis@puppet.com>
2025-10-02 18:28:18 +0100

    refactor(do52): remove default keymap and update PS2 config
    Deleted the default keymap files and related VIAL/VIA settings. Updated
    PS2 configuration in config.h and keyboard.json, including clock/data
    pin assignments and enabling PS2_PIO_USE_PIO1. Added pinout comments for
    reference.

--- keyboards/do52/config.h
+++ keyboards/do52/config.h
@@ -2,9 +2,8 @@
 #pragma once
 #define MASTER_RIGHT
 
-// #define PS2_PIO_USE_PIO1
+#define PS2_PIO_USE_PIO1
 #define PS2_MOUSE_ROTATE 90
-#define PS2_MOUSE_USE_REMOTE_MODE
 // uncomment for left hand
 //#undef PS2_MOUSE_USE_REMOTE_MODE
 
@@ -18,3 +17,8 @@
 
 
 
+// 1. GP23
+// 2. 3v3
+// 3. GP22
+// 4. Ground
+// 5. 3v3
